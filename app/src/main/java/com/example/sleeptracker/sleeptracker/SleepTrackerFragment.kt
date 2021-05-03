de945e28911e08aad823d8d3ea49778a65cfb37d
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2021-05-03 11:37:57 +0530

    adds navigation graph

--- app/src/main/java/com/example/sleeptracker/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/sleeptracker/sleeptracker/SleepTrackerFragment.kt
@@ -5,16 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.example.sleeptracker.R
+import com.example.sleeptracker.databinding.FragmentSleepTrackerBinding
 
 class SleepTrackerFragment : Fragment() {
+
+    private lateinit var binding : FragmentSleepTrackerBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sleep_tracker, container, false)
+
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_sleep_tracker, container, false)
+
+        return binding.root
     }
 
 
